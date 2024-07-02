ae34b392df8c1ada8dd23a8153a2600d30998995
Gonzalo Martinez <gonzalo.martinez.ra@gmail.com>
2024-07-02 19:14:06 -0400

    chore: add logging and json_file_path to q1_memory function

--- src/q1_memory.py
+++ src/q1_memory.py
@@ -1,6 +1,17 @@
+import logging
 from datetime import datetime
 from typing import List, Tuple
 
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
+
 
 def q1_memory(file_path: str) -> List[Tuple[datetime.date, str]]:
-    pass
+    logger.info("Start Q1 Memory function")
+    return []
+
+if __name__ == "__main__":
+    json_file_path = "data/farmers-protest-tweets-2021-2-4.json"
+    q1_memory(json_file_path)
