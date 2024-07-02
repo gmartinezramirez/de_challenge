41690173c60b9ad7717e5705635d44a6c754f337
Gonzalo Martinez <gonzalo.martinez.ra@gmail.com>
2024-07-02 19:29:28 -0400

    chore: add dataclasses that is going to be used when process the file

--- src/q1_memory.py
+++ src/q1_memory.py
@@ -1,4 +1,5 @@
 import logging
+from dataclasses import dataclass
 from datetime import datetime
 from typing import List, Tuple
 
@@ -8,7 +9,31 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
+@dataclass(frozen=True)
+class TweetUser:
+    """Representa un Twitter user con username."""
+
+    username: str
+
+
+@dataclass(frozen=True)
+class TweetData:
+    """Representa un tweet con una fecha en str y user information como TweetUser."""
+
+    date: str
+    user: TweetUser
+
+
 def q1_memory(file_path: str) -> List[Tuple[datetime.date, str]]:
+    """Procesa la info de un filepath y retorna una lista de tuplas de fechas y strings.
+    Optimizando uso de memoria
+
+    Args:
+        file_path (str): El filepath del archivo a procesar
+
+    Returns:
+        List[Tuple[datetime.date, str]]: Lista de tuplas de fechas y strings.
+    """
     logger.info("Start Q1 Memory function")
     return []
 
